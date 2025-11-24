289fca04bb892d9ede6eb3511a364dbcf8abbba4
Onni Hakala <onni@flaky.build>
2025-11-24 15:54:54 +0200

    Remove unneccessary files

--- test_no_filter.sql
+++
@@ -1,9 +0,0 @@
-SET autoinstall_known_extensions=1;
-SET autoload_known_extensions=1;
-
-LOAD 'build/debug/extension/common_crawl/common_crawl.duckdb_extension';
-
--- Test WITHOUT status_code filter to see all status codes
-SELECT url, mime_type, status_code
-FROM common_crawl_index('CC-MAIN-2025-43', '*.teamtailor.com/*', 20)
-LIMIT 10;
